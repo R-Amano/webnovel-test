98abe20dc7ff045e6e0c5529c0925e9e7a48a93e
Ryosuke Amano <tenjougun@gmail.com>
2026-05-05 09:42:41 +0900

    0.26.5.5

--- sw.js
+++ sw.js
@@ -1,11 +1,41 @@
-const CACHE_NAME = 'novel-cache-v1';
+const CACHE_NAME = 'novel-cache-v2';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './manifest.json',
+  './assets/img/icon.png',
+  './assets/img/title.png',
+  './assets/img/thumb.jpg',
+  './storys/meta.json',
+  'https://fonts.googleapis.com/css2?family=Shippori+Mincho&display=swap'
+];
 
 self.addEventListener('install', (event) => {
-  console.log('Service Worker installed');
+  // インストール時に必要なアセットをすべてキャッシュする
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  // 新しいバージョンが有効になったら古いキャッシュを削除する
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    caches.match(event.request).then((response) => {
+      // キャッシュがあればそれを返し、なければネットワークから取得する
+      return response || fetch(event.request);
+    })
   );
 });
